85c7ae5222130142a582942b82aadb5d146ee015
fmd123 <fmdavis6@gmail.com>
2017-11-26 20:17:02 -0700

    one more

---
+++ cipher.js
@@ -0,0 +1,11 @@
+
+
+
+//I need to learn how to use [...] operator!
+// function encode(str) {
+//   return [...str].map(c=>String.fromCharCode(c.charCodeAt()*6)).join('');
+// }
+//
+// function decode(str) {
+//   return [...str].map(c=>String.fromCharCode(c.charCodeAt()/6)).join('');
+// }
