739fa851bbc40c1beb827d7930e1e84e8fef9764
fmd123 <fmdavis6@gmail.com>
2018-02-07 12:54:11 -0700

    3 katas

---
+++ training-js-10.js
@@ -0,0 +1,21 @@
+// https://www.codewars.com/kata/5721a78c283129e416000999/solutions/javascript
+
+function pickIt(arr){
+  var odd=[],even=[];
+  for (i = 0; i<arr.length; i++){
+    arr[i]%2 ===0 ? even.push(arr[i]) : odd.push(arr[i]);
+  }
+  return [odd,even];
+}
+//even better:
+// function pickIt (arr) {
+//
+//   let odd = []
+//   let even =[]
+//
+//   for (var x of arr) {
+//       ((x % 2) ? odd : even).push(x)
+//   }
+//
+//   return [odd, even]
+// }
