e3489ebd5ed3ed82bbbc7b82c2a776efd74f53e3
fmd123 <fmdavis6@gmail.com>
2018-02-10 16:34:01 -0700

    white space and regex

---
+++ remove-string-spaces.js
@@ -0,0 +1,8 @@
+function noSpace(x){
+  return x.replace(/\s/g,'')
+}
+
+// noSpace('8 j 8   mBliB8g  imjB8B8  jl  B')
+//Instructions: Simple, remove the spaces from the string, then return the resultant string.
+
+//https://stackoverflow.com/questions/6623231/remove-all-white-spaces-from-text
